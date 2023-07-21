0f93a681809868d1f667c3f9224bfe1ef6f11d21
fexbard <fexbarreda@gmail.com>
2023-07-20 21:40:20 -0400

    inicio

---
+++ src/main/java/com/proyect/wolverine/WolverineApplication.java
@@ -0,0 +1,13 @@
+package com.proyect.wolverine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WolverineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WolverineApplication.class, args);
+	}
+
+}
